3a8df39990495bf3d7fda202bf8bc6413871799b
wemyir <wemyir@gmail.com>
2017-09-25 13:29:23 +0330

    add options models

--- web/admin.py
+++ web/admin.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import Posts,slider
+from models import Posts,slider,Opt
 
 admin.site.register(Posts)
 admin.site.register(slider)
+admin.site.register(Opt)
